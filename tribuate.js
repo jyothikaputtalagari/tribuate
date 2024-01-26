f55445f007dff743b88b590993e7d45af51c836d
jyothikaputtalagari <157842745+jyothikaputtalagari@users.noreply.github.com>
2024-01-27 01:29:06 +0530

    Add files via upload

---
+++ tribuate.js
@@ -0,0 +1,12 @@
+document.getElementById("toggleBiography").addEventListener("click", function () {
+    var biographySection = document.querySelector(".biography_section");
+    biographySection.classList.toggle("hidden");
+  
+    var toggleButton = document.getElementById("toggleBiography");
+    if (biographySection.classList.contains("hidden")) {
+      toggleButton.textContent = "Show Biography";
+    } else {
+      toggleButton.textContent = "Hide Biography";
+    }
+  });
+  
